2bb1e4cbc6ce20a0c08c61481292e38b18949bb5
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2024-04-16 10:29:09 +0200

    "deploy-again"

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-// login => 
+// login =>
 /*
 email : moamen@example.com
 password : 26431012
 */
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import  './styles/App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/App.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
