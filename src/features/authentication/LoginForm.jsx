8dd4c603a7f6a70ccc6dd37db2be34ce6a8784ba
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2025-04-23 11:14:05 +0200

    "final updates"

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,43 +1,44 @@
-import { useForm } from 'react-hook-form';
-import Form from '../../ui/Form';
-import FormRowVertical from '../../ui/FormRowVertical';
-import Input from '../../ui/Input';
-import Button from '../../ui/Button';
-import { useLogin } from './useLogin';
-import SpinnerMini from '../../ui/SpinnerMini';
+import { useForm } from "react-hook-form";
+import Form from "../../ui/Form";
+import FormRowVertical from "../../ui/FormRowVertical";
+import Input from "../../ui/Input";
+import Button from "../../ui/Button";
+import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const { register, handleSubmit } = useForm();
-  const {login, isLoading} = useLogin();
-  function onSubmit(data,e) {
+  const { login, isLoading } = useLogin();
+  function onSubmit(data, e) {
     e.preventDefault();
-    if (!data.email || !data.password) return null ;
-    login({email : data.email, password: data.password});
-    console.log(data)
+    if (!data.email || !data.password) return null;
+    login({ email: data.email, password: data.password });
   }
   return (
     <Form
-      type='regular'
+      type="regular"
       onSubmit={handleSubmit(onSubmit)}
-      className='min-w-[27rem] sm:min-w-[38rem]'
+      className="min-w-[27rem] sm:min-w-[38rem]"
     >
-      <FormRowVertical label='email'>
+      <FormRowVertical label="email">
         <Input
-          type='email'
-          placeholder='Enter your Email'
-          id='email'
-          {...register('email', { required: 'required field !' })}
+          type="email"
+          placeholder="Enter your Email"
+          id="email"
+          defaultValue="moamen@example.com"
+          {...register("email", { required: "required field !" })}
         />
       </FormRowVertical>
-      <FormRowVertical label='password'>
+      <FormRowVertical label="password">
         <Input
-          type='password'
-          id='password'
-          {...register('password', { required: 'required field !' })}
+          type="password"
+          id="password"
+          defaultValue="26431012"
+          {...register("password", { required: "required field !" })}
         />
       </FormRowVertical>
-      <Button size='large' disabled={isLoading}>
-        {isLoading ? <SpinnerMini /> : 'Log In'}
+      <Button size="large" disabled={isLoading}>
+        {isLoading ? <SpinnerMini /> : "Log In"}
       </Button>
     </Form>
   );
